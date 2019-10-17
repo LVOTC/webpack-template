4fc02075f5212e0acea7445eec84bb549fd76223
LVOTC <lukas@Dianas-MacBook-Air.local>
2019-10-17 21:22:14 +0300

    config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './assets/style.css';
+import './assets/style/style.scss';
+import Icon from './assets/images/menu.svg';
+
+const section = document.createElement('div');
+document.body.appendChild(section);
+
+const img = new Image()
+img.src = Icon
+section.appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 var path = require('path');
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
@@ -9,12 +17,34 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
-          'style-loader',
-           'css-loader'
+          {
+             loader: MiniCssExtractPlugin.loader,
+             options: {
+               publicPath: (resourcePath, context) => {
+                 return path.relative(path.dirname(resourcePath), context) + '/';
+               },
+             },
+           },
+          'css-loader',
+          'sass-loader',
          ]
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+    filename: '[name].css',
+  }),
+  ]
 }
